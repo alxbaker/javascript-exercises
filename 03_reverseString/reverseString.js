962ab4167e72fb5591335f53dccdd989d8fc91d8
Alex Baker <37917867+alxbaker@users.noreply.github.com>
2022-12-17 20:10:55 -0600

    Return strings in reverse order

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    characters = string.split('');
+    let reversedString = '';
+    while (characters.length > 0) {
+        reversedString += characters.pop()
+    }
+    return reversedString
 };
 
 // Do not edit below this line
