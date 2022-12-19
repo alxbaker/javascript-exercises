2183a29e0373cd105072ae35fee9255d41906ad1
Alex Baker <37917867+alxbaker@users.noreply.github.com>
2022-12-18 20:00:12 -0600

    Sum all numbers in a range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    finalSum = 0;
+    if ((typeof num1 !== "number") || (typeof num2 !== "number")) {
+        return 'ERROR';
+    } else if ((num1 < 0) || (num2 < 0)) {
+        return 'ERROR';
+    } else {
+        max = Math.max(num1, num2);
+        min = Math.min(num1, num2);
+        for (let i = min; i<=max; i++) {
+            finalSum += i;
+        }
+        return finalSum;
+    }
 };
 
 // Do not edit below this line
