40322a8ec32601cc7e451e13c46d659d5b9cd78e
Alex Baker <37917867+alxbaker@users.noreply.github.com>
2023-02-28 21:12:43 -0600

    Add, subtract, multiply, exponentiate, and factorialize numbers

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const sum = function(a) {
+	total = 0;
+  for (const number of a) {
+    total += number;
+  }
+  return total;
+};
+
+const multiply = function(a) {
+	total = 0;
+  for (const number of a) {
+    if (total > 0) {
+      total *= number;
+    } else {
+      total = number;
+    }
+  }
+  return total;
+};
+
+const power = function(a, b) {
+  return Math.pow(a, b);
 	
 };
 
-const subtract = function() {
-	
-};
+const factorial = function(a) {
+  if (a < 0) {
+    return -1;
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+  } else if (a == 0) {
+    return 1;
+  } else {
+    return (a * factorial(a - 1));
+  }
 };
 
 // Do not edit below this line
