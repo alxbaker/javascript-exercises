8bf52e04dfc828d284d7b224872b4b1c6192e580
Alex Baker <37917867+alxbaker@users.noreply.github.com>
2023-02-28 21:39:05 -0600

    Create case/punctuation insensitive palindrome function for multiple words

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    cleanPhrase = phrase.toLowerCase().replace(/[^a-z]/g, "");
+    revPhrase = cleanPhrase.split("").reverse().join("");
+    return cleanPhrase == revPhrase;
 };
 
 // Do not edit below this line
