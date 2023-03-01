3c5c3d34f406b05bbd8a1aba2f5cac8b8cd1b68f
Alex Baker <37917867+alxbaker@users.noreply.github.com>
2023-02-28 22:41:12 -0600

    Create function to find the oldest person in an array of objects

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        if ("yearOfDeath" in oldest) {
+            currentOldest = oldest.yearOfDeath - oldest.yearOfBirth;
+        } else {
+            currentOldest = new Date().getFullYear() - oldest.yearOfBirth;            
+        }
+        if ("yearOfDeath" in person) {
+            current = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            current = new Date().getFullYear()- person.yearOfBirth;
+        }
+        if (current > currentOldest) {
+            return person;
+        } else {
+            return oldest;
+        }
+    })
 };
 
 // Do not edit below this line
